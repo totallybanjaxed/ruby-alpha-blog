4d3ed86b6d022b7a6c29dce917097a6779f98a78
Totally Banjaxed <totallybanjaxed@gmail.com>
2022-02-09 00:29:16 +0000

    validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
 end
